81059d50de1af75a3f3b205a002f7d38373c5d4d
Isaac Rezey <isaacrez20@gmail.com>
2021-01-07 17:36:56 -0500

    Implement timer functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
+import useInterval from './useInterval';
 import './App.css';
 
+const intervalSeconds = 25 * 60;
+const cooldownSeconds = 5 * 60;
+
 function App() {
+  const [time, setTime] = React.useState(intervalSeconds);
+  const [laps, setLaps] = React.useState(0);
+  const [isRunning, setIsRunning] = React.useState(true);
+  const [isCoolDown, setCoolDown] = React.useState(false);
+
+  function clickHandle() {
+    setIsRunning(isRunning => !isRunning);
+  }
+
+  function decrementTime() {
+    setTime(prevTime => prevTime - 1);
+  }
+
+  function incrementLaps() {
+    setLaps(prevLaps => prevLaps + 1);
+  }
+
+  function getFormattedTime() {
+    let seconds = time;
+    let minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+
+    minutes = minutes < 10 ? "0" + minutes : "" + minutes;
+    seconds = seconds < 10 ? "0" + seconds : "" + seconds;
+
+    return minutes + ':' + seconds;
+  }
+
+  useInterval(decrementTime, (isRunning || isCoolDown) ? 1000 : null);
+
+  React.useEffect(resetClockIfNecessary, [time]);
+
+  function resetClockIfNecessary() {
+    if (time <= 0) {
+      let newTime = isCoolDown ? intervalSeconds : cooldownSeconds;
+      setTime(newTime)
+
+      isCoolDown || incrementLaps();
+      setCoolDown(prevCoolDown => !prevCoolDown);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="timer">
+        <h1 className={isCoolDown ? "coolDown" : null}>
+          {getFormattedTime()}
+        </h1>
+      </div>
+
+      <h2>Laps: {laps}</h2>
+      
+      <button
+        onClick={clickHandle}>
+        {isRunning ? "STOP" : "START"}
+      </button>
     </div>
   );
 }
